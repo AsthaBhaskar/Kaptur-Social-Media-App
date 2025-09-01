507a7cc2cf2af2640a30bcc725f5a781d26c8160
astha bhaskar <asthabhaskar30@gmail.com>
2025-09-01 20:34:34 +0530

    Update backend

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://your-vercel-domain.vercel.app", // Replace with your actual Vercel domain
+    "https://kaptur-social-media-app.vercel.app/", // Replace with your actual Vercel domain
     "https://*.vercel.app" // Allow all Vercel subdomains
   ],
   credentials: true,
